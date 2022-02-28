1c8fe784c2fffb50c1d344ab8dc8f6cfc827b3bd
Andy <59408071+CS-Andy@users.noreply.github.com>
2022-02-28 01:14:27 -0800

    Update and rename AHSOFT.py to testing.py

--- AHSOFT.py
+++
@@ -1,5 +0,0 @@
-# this is a test 2021.
-name = input("Enter your name please?")
-print (name.title())
-print (name.upper())
-print (name.casefold())
---
+++ testing.py
@@ -0,0 +1,14 @@
+# this is a test 2021.
+name = input("Enter your name please?")
+print (name.title())
+print (name.upper())
+print (name.casefold())
+
+name = input("Enter your name, please? ")
+print ("Hello %s" % name.title())
+
+age = input("And your age? ")
+print ("Thank you.")
+
+print("OK, so your name is " % name.title())
+"and your age is " % age)

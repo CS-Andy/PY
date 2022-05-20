77ed703725bbbbd7c44e5ce1359148628b531c13
Andy <59408071+CS-Andy@users.noreply.github.com>
2022-05-20 14:55:25 -0700

    Add files via upload

---
+++ HW01.py
@@ -0,0 +1,29 @@
+'''
+message = 'hello world'
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+fname = "andy"
+lname = "huynh"
+fullname = f"Hello, {fname} {lname}!"
+print(fullname.title())
+
+print("Name:\n\tFennie\n\tAndy\n\tFiona")
+
+name = ' python '
+print(name.rstrip())
+' python'
+print(name.lstrip())
+'python '
+print(name.strip())
+'python'
+
+message = 'Hello it\'s me '
+print(message)
+'''
+
+name = 'Eric'
+print (name.lower())
+
+
